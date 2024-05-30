a00b256d2f62fcd7ad79e5593b421ed61f918601
randaaz <randaaziz972@gmail.com>
2024-05-29 23:24:37 -0700

    mplementing a class named ClassRoom

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,13 @@
+/**
+ * Class representing a classroom.
+ */
+export default class ClassRoom {
+  /**
+   * Create a classroom.
+   * @param {Number} maxStudentsSize - The maximum number of students
+   * allowed in the classroom.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
